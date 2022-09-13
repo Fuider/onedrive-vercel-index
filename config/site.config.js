6af829ebca04925b7285b7af74e014b8133c93d2
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2022-09-13 21:12:06 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -2,6 +2,7 @@
  * This file contains the configuration used for customising the website, such as the folder to share,
  * the title, used Google fonts, site icons, contact info, etc.
  */
+// rediss://:cf5d01cedc13424f872fc37c7427601e@global-smart-bream-31459.upstash.io:31459
 module.exports = {
   // This is what we use to identify who you are when you are initialising the website for the first time.
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
