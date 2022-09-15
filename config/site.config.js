6d68c10dbe2e3c851fe80404510162c251481ede
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2022-09-15 21:39:10 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -18,7 +18,7 @@ module.exports = {
   title: "人翠内部文件",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/cao_cloud',
+  baseDirectory: '/2班',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -36,23 +36,20 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '曹智铭的云盘，曹智铭。',
+    '人大附中翠微学校，版权所有。',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:czm@czhiming.cn',
+  
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/cao-zhiming',
-    },
+   
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
